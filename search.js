4e056825bb22699c999a447a00e33102dbc752a0
TacoBlayno <110860860+TacoBlayno@users.noreply.github.com>
2023-01-29 13:48:06 -0800

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,6 @@
 var searchEl = document.getElementById("search");
+var searchElDiv = document.getElementById("search-div");
+
 searchEl.addEventListener("keydown", function(event) {
   if (event.keyCode === 13) {
     search(searchEl.value);
@@ -6,9 +8,16 @@ searchEl.addEventListener("keydown", function(event) {
 });
 
 function search(value) {
+  values = value.split(" ");
+  delete value;
+  
+  searchElDiv.innerHTML = "";
   let webPages = {
     "Minecraft" : {
-      "Home" : {},
+      "Home" : {
+        "src" : "https://tacoblayno.github.io/minecraft/home",
+        "description" : "",
+      },
       "Parkour Generator" : {},
       "Shape Comands" : {},
     },
@@ -18,6 +27,25 @@ function search(value) {
     //"" : {},
   };
   
-  if (webPages[value]) {
+  function searchKeys(value, object) {
+    if (object[values[0]]) {
+      if (typeof(object[values[0]]) === "object") {
+        let newValue;
+        for (index = 1; index < values.length; index++) {
+          newValue.push(values[index]);
+        }
+        if (searchKeys(newValue, values[0]]) === false) {
+          document.write();
+          object["src"]
+          return true;
+        } else {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
   }
+  
+  searchKeys(values, webPages)
 }
