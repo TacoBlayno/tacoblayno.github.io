dd0597b9d4816c7d775af2fb0cbd38ab6f2bcd53
TacoBlayno <110860860+TacoBlayno@users.noreply.github.com>
2023-01-29 16:28:59 -0800

    Update search.js

--- search.js
+++ search.js
@@ -43,7 +43,7 @@ function search(value) {
             return true;
           } else {
             let searchElP = document.createElement("p");
-            searchElP.appendChild(document.createTextNode(object["discription"] + string(object) + object));
+            searchElP.appendChild(document.createTextNode(object["discription"] + object));
             searchElDiv.appendChild(searchElP);
             return true;
           }
