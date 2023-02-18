05dd67da1615afcc2360d3b4866f6ea2ae1423b7
TacoBlayno <110860860+TacoBlayno@users.noreply.github.com>
2023-02-18 11:55:29 -0800

    Update elements.js

--- base/elements.js
+++ base/elements.js
@@ -1,13 +1,14 @@
 let headerEl = document.createElement("header");
-let headerAEl = document.createElement("a")
+let headerAEl = document.createElement("a");
 headerAEl.appendChild(document.createTextNode("TacoBlayno's Website"));
+headerAEl.src = "/home.html"
 headerEl.appendChild(headerAEl);
 document.body.appendChild(headerEl);
 
-document.body.addEventListener("load", function() {
+function bodyLoaded() {
   let footerEl = document.createElement("footer");
   let thanksEl = this.document.createElement("a");
   thanksEl.appendChild(document.createTextNode("Thanks"));
   footerEl.appendChild(thanksEl);
   document.body.appendChild(footerEl);
-});
+}
