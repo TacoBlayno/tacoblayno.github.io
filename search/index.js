18d6924ab9c286cb3c6458403939b970bd824d24
TacoBlayno <blaynestegman@outlook.com>
2023-02-19 18:26:08 -0800

    Major Site Navigation Update

--- search/index.js
+++ search/index.js
@@ -1,37 +1,65 @@
 class DisplayTags {
   constructor(xmlDoc) {
     this._xmlDoc = xmlDoc;
-    this._pages = pagesValues(this._xmlDoc.documentElement);
-    console.log(this._pages);
-  }
-  get pagesValues() {
-    return this._pages;
-  }
-  set pagesValues(xmlDoc) {
-    let map = new Map();
-    if (xmlDoc.childNodes.length > 1) {
-      let array = [];
-      for (const element of xmlDoc.childNodes) {
-        if (element.nodeType == 1) {
-          array.push(this._getPagesValues(element));
+    this._pagesValues = function(xmlDoc) {
+      let map = new Map();
+      if (xmlDoc.childNodes.length > 1) {
+        let array = [];
+        for (const element of xmlDoc.childNodes) {
+          if (element.nodeType == 1) {
+            array.push(this._getPagesValues(element));
+          }
+        }
+        map.set(xmlDoc, array);
+      } else {
+        if (xmlDoc.firstChild.nodeType == 3) {
+          map.set(xmlDoc, xmlDoc.firstChild);
+        } else if (xmlDoc.firstChild.nodeType == 1) {
+          map.set(xmlDoc, [this._getPagesValues(xmlDoc.firstChild)]);
         }
       }
-      map.set(xmlDoc, array);
-    } else {
-      if (xmlDoc.firstChild.nodeType == 3) {
-        map.set(xmlDoc, xmlDoc.firstChild);
-      } else if (xmlDoc.firstChild.nodeType == 1) {
-        map.set(xmlDoc, this._getPagesValues(xmlDoc.firstChild));
-      }
+      return map;
     }
-    return map;
+    this._pages = this._pagesValues(this._xmlDoc.documentElement);
+    console.log(this._pages);
+  }
+  get pages() {
+    return this._pages;
+  }
+  set pages(xmlDoc) {
+    this._pages = this._pagesValues(xmlDoc);
   }
 }
 
 let searchEl = document.getElementById("search-bar");
 let viewAllEl = document.getElementById("view-all");
 let searchElDiv = document.getElementById("search-div");
-let veiwAlElDiv = document.getElementById("view-all-div");
+let viewAllElDiv = document.getElementById("view-all-div");
+
+function mapFolder(map) {
+  for (const element of map.keys()) {
+    let ulEl = document.createElement("ul");
+    let liEl = document.createElement("li");
+    liEl.appendChild(document.createTextNode(element));
+    liEl.addEventListener("mouseup", function() {
+      if (liEl.style.borderLeft != "none") {
+        liEl.style.borderLeft = "3px solid #f0f";
+        if (Array.isArray(map.get(element))) {
+          liEl.appendChild(mapFolder(element));
+        } else if (typeof map.get(element) == "string") {
+          liEl.appendChild(map.get(element));
+        }
+      } else {
+        liEl.style.borderLeft = "none";
+        for (let i = 1; i < liEl.children; i++) {
+          liEl.removeChild(liEl.children[i]);
+        }
+      }
+    });
+    ulEl.appendChild(liEl);
+    return ulEl;
+  }
+}
 
 searchEl.addEventListener("keydown", function(event) {
   search(searchEl.value); });
@@ -68,6 +96,7 @@ function viewAll() {
   xmlRequest.addEventListener("load", function (values) {
     if (this.readyState == 4 && this.status == 200) {
       displayTags = new DisplayTags(xmlRequest.responseXML);
+      viewAllElDiv.appendChild(mapFolder(displayTags));
     }
   });
 
