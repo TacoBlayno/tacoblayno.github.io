64d8946e7eec0c01d9e3ac05d25ffde6d0c3a98f
TacoBlayno <110860860+TacoBlayno@users.noreply.github.com>
2023-02-20 12:04:50 -0800

    Update elements.js

--- base/elements.js
+++ base/elements.js
@@ -1,12 +1,13 @@
 let headerEl = document.createElement("header");
 let headerAEl = document.createElement("a");
 headerAEl.appendChild(document.createTextNode("TacoBlayno's Website"));
-headerAEl.src = "/home.html"
+headerAEl.href = "/home.html"
 headerEl.appendChild(headerAEl);
 document.body.appendChild(headerEl);
 
 let footerEl = document.createElement("footer");
 let thanksEl = this.document.createElement("a");
 thanksEl.appendChild(document.createTextNode("Thanks"));
+thanksEl.href = "/thanks.html"
 footerEl.appendChild(thanksEl);
 document.body.appendChild(footerEl);
